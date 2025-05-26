d7add948567d65921b9b3df9621734e9189907c9
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-27 00:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function calculateGrade(percentage) {
   //your code here
+	if(percentage>=90){
+		return 'A';
+	}
+	if else(percentage<=89 && percentage>=80){
+		return 'B';
+	}
+	if else(percentage<=79 && percentage>=70){
+		return 'C';
+	}
+	if else(percentage<=69 && percentage>=60){
+		return 'D';
+	}
+	if else(percentage<60){
+		return 'F';
+	}
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
